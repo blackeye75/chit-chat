9dc3f0ae0b7780fe31824822c1c86f0686d526a0
blackeye75 <priyanshuraj22275@gmail.com>
2025-07-01 22:27:42 +0530

    fix: update theme variable in App component for consistent styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
 
   return (
-    <div className='h-screen' data-theme={dark}>
+    <div className='h-screen' data-theme={theme}>
       <Routes>
         <Route
           path="/"
